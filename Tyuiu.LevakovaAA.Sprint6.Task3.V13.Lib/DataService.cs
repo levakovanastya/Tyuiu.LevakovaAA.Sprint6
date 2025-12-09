e81c44731bac6adb944da6b70aedae7644d2976e
Настя Левакова <tyuiu.levakovaaa@outlook.com>
2025-12-09 19:55:44 +0500

    Task3

--- Tyuiu.LevakovaAA.Sprint6.Task3.V13.Lib/DataService.cs
+++ Tyuiu.LevakovaAA.Sprint6.Task3.V13.Lib/DataService.cs
@@ -14,38 +14,18 @@ namespace Tyuiu.LevakovaAA.Sprint6.Task3.V13.Lib
             int cols = matrix.GetLength(1);
 
             int[,] resultMatrix = (int[,])matrix.Clone();
-
-            SortByColumn(resultMatrix, 1);
+            int[] secondColumn = new int[rows];
+            for (int i = 0; i < rows; i++)
+            {
+                secondColumn[i] = resultMatrix[i, 1];
+            }
+            Array.Sort(secondColumn);
+            for (int i = 0; i < rows; i++)
+            {
+                resultMatrix[i, 1] = secondColumn[i];
+            }
 
             return resultMatrix;
-        }
-         
-
-        private void SortByColumn(int[,] matrix, int columnIndex)
-        {
-            int rows = matrix.GetLength(0);
-            for (int i = 0; i < rows - 1; i++)
-            {
-                for (int j = 0; j < rows - i - 1; j++)
-                {
-                    if (matrix[j, columnIndex] > matrix[j + 1, columnIndex])
-                    {
-                        SwapRows(matrix, j, j + 1);
-                    }
-                }
-            }
-        }
-
-        private void SwapRows(int[,] matrix, int row1, int row2)
-        {
-            int cols = matrix.GetLength(1);
-
-            for (int col = 0; col < cols; col++)
-            {
-                int temp = matrix[row1, col];
-                matrix[row1, col] = matrix[row2, col];
-                matrix[row2, col] = temp;
-            }
         }
     }
 }
